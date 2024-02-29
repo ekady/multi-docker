c192750c87f2f1b3b1821f0f0f915b2666cdd7e0
dyah-eka <dyahekalestari9@gmail.com>
2024-02-29 08:57:29 +0700

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
@@ -10,8 +9,6 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
           <a
             className="App-link"
             href="https://reactjs.org"
